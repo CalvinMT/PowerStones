b91c7c6216d793e9585256c990fcbe5dc9ffb2e3
Calvin M.T <calvin.massonnet@gmail.com>
2024-01-06 13:12:19 +0100

    Fix diode blocks alternate input signal

--- fabric/src/main/java/com/calvinmt/powerstones/mixin/AbstractRedstoneGateBlockMixin.java
+++ fabric/src/main/java/com/calvinmt/powerstones/mixin/AbstractRedstoneGateBlockMixin.java
@@ -2,20 +2,67 @@ package com.calvinmt.powerstones.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.calvinmt.powerstones.PowerStones;
 import com.calvinmt.powerstones.WorldInterface;
+import com.calvinmt.powerstones.block.MultipleWiresBlock;
+import com.calvinmt.powerstones.block.PowerstoneWireBlockBase;
 
 import net.minecraft.block.AbstractRedstoneGateBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
 
 @Mixin(AbstractRedstoneGateBlock.class)
-public abstract class AbstractRedstoneGateBlockMixin {
+public abstract class AbstractRedstoneGateBlockMixin extends HorizontalFacingBlock {
+
+    protected AbstractRedstoneGateBlockMixin(Settings settings) {
+        super(settings);
+    }
 
     @Redirect(method = "getPower(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getEmittedRedstonePower(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)I"))
     private int getPowerGetEmittingPower(World world, BlockPos pos, Direction direction) {
         return ((WorldInterface) world).getMaxPower(pos, direction);
+    }
+
+    @Inject(method = "getPower(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)I", at = @At("TAIL"), cancellable = true)
+    public void getPowerReturn(World world, BlockPos pos, BlockState state, CallbackInfoReturnable<Integer> callbackInfo) {
+        Direction direction = (Direction)state.get(FACING);
+        BlockPos blockPos = pos.offset(direction);
+        BlockState blockState = world.getBlockState(blockPos);
+        int power = 0;
+        if (blockState.isOf(PowerStones.BLUESTONE_WIRE) || blockState.isOf(PowerStones.GREENSTONE_WIRE) || blockState.isOf(PowerStones.YELLOWSTONE_WIRE)) {
+            power = blockState.get(PowerstoneWireBlockBase.POWER);
+        }
+        else if (blockState.isOf(PowerStones.MULTIPLE_WIRES)) {
+            power = Math.max(blockState.get(MultipleWiresBlock.POWER), blockState.get(MultipleWiresBlock.POWER_B));
+        }
+        callbackInfo.setReturnValue(Math.max(callbackInfo.getReturnValue(), power));
+    }
+
+    @Inject(method = "getInputLevel(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/AbstractRedstoneGateBlock;isValidInput(Lnet/minecraft/block/BlockState;)Z", shift = At.Shift.AFTER), cancellable = true)
+    public void getInputLevelConditions(WorldView world, BlockPos pos, Direction dir, CallbackInfoReturnable<Integer> callbackInfo) {
+        BlockState blockState = world.getBlockState(pos);
+        if (blockState.isOf(PowerStones.BLUESTONE_BLOCK) || blockState.isOf(PowerStones.GREENSTONE_BLOCK) || blockState.isOf(PowerStones.YELLOWSTONE_BLOCK)) {
+            callbackInfo.setReturnValue(15);
+        }
+        else if (blockState.isOf(PowerStones.BLUESTONE_WIRE) || blockState.isOf(PowerStones.GREENSTONE_WIRE) || blockState.isOf(PowerStones.YELLOWSTONE_WIRE)) {
+            callbackInfo.setReturnValue(blockState.get(PowerstoneWireBlockBase.POWER));
+        }
+        else if (blockState.isOf(PowerStones.MULTIPLE_WIRES)) {
+            callbackInfo.setReturnValue(Math.max(blockState.get(MultipleWiresBlock.POWER), blockState.get(MultipleWiresBlock.POWER_B)));
+        }
+    }
+
+    @Redirect(method = "getInputLevel(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldView;getStrongRedstonePower(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;)I"))
+    public int getInputLevelGetStrongPower(WorldView world, BlockPos pos, Direction direction) {
+        return Math.max(world.getStrongRedstonePower(pos, direction), Math.max(world.getStrongBluestonePower(pos, direction), Math.max(world.getStrongGreenstonePower(pos, direction), world.getStrongYellowstonePower(pos, direction))));
     }
 
 }
--- forge/src/main/java/com/calvinmt/powerstones/mixin/DiodeBlockMixin.java
+++ forge/src/main/java/com/calvinmt/powerstones/mixin/DiodeBlockMixin.java
@@ -2,20 +2,68 @@ package com.calvinmt.powerstones.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import com.calvinmt.powerstones.LevelInterface;
+import com.calvinmt.powerstones.LevelReaderInterface;
+import com.calvinmt.powerstones.PowerStones;
+import com.calvinmt.powerstones.block.MultipleWiresBlock;
+import com.calvinmt.powerstones.block.PowerstoneWireBlockBase;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.DiodeBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
 
 @Mixin(DiodeBlock.class)
-public abstract class DiodeBlockMixin {
+public abstract class DiodeBlockMixin extends HorizontalDirectionalBlock {
+
+    protected DiodeBlockMixin(Properties properties) {
+        super(properties);
+    }
 
     @Redirect(method = "getInputSignal(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getSignal(Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;)I"))
     private int getInputSignalGetEmittingSignal(Level level, BlockPos pos, Direction direction) {
         return ((LevelInterface) level).getMaxSignal(pos, direction);
+    }
+
+    @Inject(method = "getInputSignal(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)I", at = @At("TAIL"), cancellable = true)
+    public void getPowerReturn(Level level, BlockPos pos, BlockState state, CallbackInfoReturnable<Integer> callbackInfo) {
+        Direction direction = (Direction)state.getValue(FACING);
+        BlockPos blockPos = pos.relative(direction);
+        BlockState blockState = level.getBlockState(blockPos);
+        int power = 0;
+        if (blockState.is(PowerStones.BLUESTONE_WIRE.get()) || blockState.is(PowerStones.GREENSTONE_WIRE.get()) || blockState.is(PowerStones.YELLOWSTONE_WIRE.get())) {
+            power = blockState.getValue(PowerstoneWireBlockBase.POWER);
+        }
+        else if (blockState.is(PowerStones.MULTIPLE_WIRES.get())) {
+            power = Math.max(blockState.getValue(MultipleWiresBlock.POWER), blockState.getValue(MultipleWiresBlock.POWER_B));
+        }
+        callbackInfo.setReturnValue(Math.max(callbackInfo.getReturnValue(), power));
+    }
+
+    @Inject(method = "getAlternateSignalAt(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/DiodeBlock;isAlternateInput(Lnet/minecraft/world/level/block/state/BlockState;)Z", shift = At.Shift.AFTER), cancellable = true)
+    public void getAlternateSignalAtConditions(LevelReader level, BlockPos pos, Direction dir, CallbackInfoReturnable<Integer> callbackInfo) {
+        BlockState blockState = level.getBlockState(pos);
+        if (blockState.is(PowerStones.BLUESTONE_BLOCK.get()) || blockState.is(PowerStones.GREENSTONE_BLOCK.get()) || blockState.is(PowerStones.YELLOWSTONE_BLOCK.get())) {
+            callbackInfo.setReturnValue(15);
+        }
+        else if (blockState.is(PowerStones.BLUESTONE_WIRE.get()) || blockState.is(PowerStones.GREENSTONE_WIRE.get()) || blockState.is(PowerStones.YELLOWSTONE_WIRE.get())) {
+            callbackInfo.setReturnValue(blockState.getValue(PowerstoneWireBlockBase.POWER));
+        }
+        else if (blockState.is(PowerStones.MULTIPLE_WIRES.get())) {
+            callbackInfo.setReturnValue(Math.max(blockState.getValue(MultipleWiresBlock.POWER), blockState.getValue(MultipleWiresBlock.POWER_B)));
+        }
+    }
+
+    @Redirect(method = "getAlternateSignalAt(Lnet/minecraft/world/level/LevelReader;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/LevelReader;getDirectSignal(Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;)I"))
+    public int getAlternateSignalAtGetStrongPower(LevelReader level, BlockPos pos, Direction direction) {
+        return Math.max(level.getDirectSignal(pos, direction), Math.max(((LevelReaderInterface) level).getDirectSignalBlue(pos, direction), Math.max(((LevelReaderInterface) level).getDirectSignalGreen(pos, direction), ((LevelReaderInterface) level).getDirectSignalYellow(pos, direction))));
     }
 
 }
